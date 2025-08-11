d7cb6038c366c2dc62e284a006d0ebb34660bf35
AlGAHBOUZ <sajed.samer.2004@gmail.com>
2025-08-11 16:15:43 +0300

    Updates

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+DATA_PATH = Path("data")
+
+GPC_PATH = DATA_PATH / "gpc_sample.xlsx"
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,6 @@
 import pandas as pd
 
+from constants import GPC_PATH
 
-def load_gpc_to_classes(GPC_PATH):
-    df = pd.read_excel(GPC_PATH)
 
-    df["class_name"] = (
-        df["BrickTitle"].fillna("") + " - " +
-        df["AttributeTitle"].fillna("") + " - " +
-        df["AttributeValueTitle"].fillna("")
-    )
 
-    def join_non_empty(*args):
-        return " ".join([str(a).strip() for a in args if pd.notna(a) and str(a).strip()])
-
-    df["description"] = df.apply(lambda row: join_non_empty(
-        row["BrickDefinition_Includes"],
-        row["BrickDefinition_Excludes"],
-        row["AttributeDefinition"],
-        row["AttributeValueDefinition"]
-    ), axis=1)
