2e290c2943e6bb374ddcd8a68b6183f55e5b6aff
AlGAHBOUZ <75996768+AlGAHBOUZ@users.noreply.github.com>
2025-08-11 14:19:26 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,44 @@
 import json
 from pathlib import Path
-from teradata_db import TeradataDatabase  # Assuming your class is saved here
+from teradata_db import TeradataDatabase
+
+def remove_strings(text: str, strings: List[str]) -> str:
+    for s in strings:
+        s = str(s)
+        if s in text:
+            text = text.replace(s, "")
+
+    return text
+
+def remove_numbers(text: str, remove_string: bool = False) -> str:
+    text = text.split()
+    text = [t for t in text if not re.search(r"\d", t)] if remove_string else [re.sub(r"\d+", "", t) for t in text]
+
+    return " ".join(text)
+
+def remove_stopwords(text: str):
+    text = text.split()
+    text = [t for t in text if t not in ALL_STOPWORDS or t == "can"]
+
+    return " ".join(text)
+
+def remove_punctuations(text: str) -> str:
+    text = re.sub(r'[^\w\s]', '', text)
+
+    return " ".join(text.strip().split()) 
+
+def clean_text(row) -> str:
+    text = row.Item_Name
+    brand = row.Brand
+    unit = str(row.Unit)
+    text = remove_strings(text, [brand])
+    text = remove_punctuations(text)
+    text = remove_numbers(text)
+    text = remove_stopwords(text)
+    if unit not in text and text == "":
+        text += unit
+
+    return  text
 
 def get_text(*fields):
     return " ".join([f.strip() for f in fields if f and f.strip()])
